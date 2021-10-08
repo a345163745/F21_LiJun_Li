6b43f61d2c05a9a148c5d074848a4003c369c5fc
a345163745 <0oalexlio0@gmail.com>
2021-10-07 22:26:04 -0400

    added glfw file

--- Boba/BobaApp.cpp
+++ Boba/BobaApp.cpp
@@ -1,13 +1,19 @@
 #include "pch.h"
 #include "BobaApp.h"
+#include "GLFW/glfw3.h"
 namespace Boba
 {
 	void BobaApp::Run()
 	{
 		std::cout << "Boba running..." << std::endl;
+		GLFWwindow* window;
+		glfwInit();
+		window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
 		while (true)
 		{
 			OnUpdate();
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 		}
 	}
 	void BobaApp::OnUpdate(){}
